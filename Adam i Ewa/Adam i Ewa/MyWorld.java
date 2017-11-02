5e6688e48c2fbef710f27aff75976ea986b2e5f5
Kakajaszi <Kakajaszi>
2017-11-02 15:24:35 +0100

    zmiana
    zmiana 2

--- Adam i Ewa/Adam i Ewa/MyWorld.java
+++ Adam i Ewa/Adam i Ewa/MyWorld.java
@@ -39,4 +39,40 @@ public class MyWorld extends World
         Wc wc = new Wc();
         addObject(wc,659,263);
     }
+    public void act()
+    {
+        //wykonywane w kółko
+        if(Greenfoot.isKeyDown("k"))
+        {
+            //1 wylosowac polozenie weza
+            int szerokosc= getWidth();
+            int wysokosc= getHeight();
+            
+            //2 umiescic weza na swiecie
+            
+            //2.1 ile wezy
+             String odp = Greenfoot.ask("Jak tam twoja szaciura w skali od 1 do 100?");
+             int ile = Integer.parseInt(odp);
+             
+            //2/2 dodaj wezna w podanej liczbie
+            for(int i=0; i<ile;i++)
+            {
+                int x= Greenfoot.getRandomNumber (szerokosc);
+                int y =Greenfoot.getRandomNumber (wysokosc);
+                Snake snake= new Snake();
+                addObject(snake,x,y);
+                //Sprawdz czy nie dotyka adam jezeli dotyka 
+                //to przenies w inne miejsce
+                while(snake.dotykaAdama()   || snake.dotykaEwe() )
+                {
+                x= Greenfoot.getRandomNumber (szerokosc);
+                y =Greenfoot.getRandomNumber (wysokosc);
+                snake.setLocation (x,y);
+                }
+                
+            }
+            
+            
+        }
+    }
 }
